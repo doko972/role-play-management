421a75eb29f5fe6335fd7704915f13154487abda
doko972 david <doko972@gmail.com>
2024-07-10 09:07:51 +0200

    first commit today

--- app/event/header.php
+++ app/event/header.php
@@ -8,14 +8,15 @@
                 <span class="line line3"></span>
             </div>
             <ul class="menu-items">
-                <li><a class="btn-add-event--register--nav" href="index.php">Accueil</a></li>
-                <li><a class="btn-add-event--register--nav" href="news.php">News</a></li>
-                <li><a class="btn-add-event--register--nav" href="saint.php">Athena</a></li>
-                <li><a class="btn-add-event--register--nav" href="marinas.php">Poseidon</a></li>
-                <li><a class="btn-add-event--register--nav" href="spectres.php">Hades</a></li>
+                <li><a class="btn-add-event--register--nav" href="../index.php">Accueil</a></li>
+                <li><a class="btn-add-event--register--nav" href="../news.php">News</a></li>
+                <li><a class="btn-add-event--register--nav" href="../saint.php">Athena</a></li>
+                <li><a class="btn-add-event--register--nav" href="../marinas.php">Poseidon</a></li>
+                <li><a class="btn-add-event--register--nav" href="../spectres.php">Hades</a></li>
+                <!-- <li><a class="btn-add-event--register--nav" href="#">Calendrier</a></li> -->
                 <li><a class="btn-add-event--register--nav" href="https://discord.gg/3zkTwdDnhc">Discord</a></li>
                 <li><a class="btn-add-event--register--nav" href="#">Connexion</a></li>
-                <li><a class="btn-add-event--register--nav" href="account.php">Accès au jeu</a></li>
+                <li><a class="btn-add-event--register--nav" href="../account.php">Accès au jeu</a></li>
             </ul>
         </div>
     </nav>
