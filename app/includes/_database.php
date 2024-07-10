bb630c02c63065d69741889d4fd07182cb7020fa
doko972 david <doko972@gmail.com>
2024-07-10 11:46:29 +0200

    correct destination file from composer v

--- app/includes/_database.php
+++ app/includes/_database.php
@@ -1,6 +1,5 @@
 <?php
 // var_dump($_ENV);
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
@@ -17,22 +16,3 @@ try {
 } catch (Exception $e) {
     die('ERREUR CONNEXION MYSQL' . $e->getMessage());
 }
-
-// var_dump($_ENV);
-
-// try {
-//     $dbConnect = new PDO(
-//         'mysql:host=db;
-//             dbname=roleplay;
-//             charset=utf8',
-//         'tama',
-//         'tekmate'
-//     );
-//     $dbConnect->setAttribute(
-//         PDO::ATTR_DEFAULT_FETCH_MODE,
-//         PDO::FETCH_ASSOC
-//     );
-
-// } catch (Exception $e) {
-//     die('Erreur connexion mysql' . $e->getMessage());
-// }
