85ac47040614f68c9f3157bf3a6ba6a12818f489
doko972 david <doko972@gmail.com>
2024-08-27 12:53:19 +0200

    first commit today

--- app/welcome.php
+++ app/welcome.php
@@ -44,7 +44,7 @@ $welcome_message = isset($_SESSION['welcome_message']) ? $_SESSION['welcome_mess
             <script>
               setTimeout(function () {
                 window.location.href = 'index.php';
-              }, 4000);
+              }, 40000);
             </script>
           </div>
         </div>
